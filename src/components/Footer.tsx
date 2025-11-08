889367e7f0f5d275cc95143939e2918d148226c0
smithwrld <smxthwrld@gmail.com>
2025-11-08 17:05:31 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -261,7 +261,7 @@ const Footer = () => {
               </div>
               
               <div className="text-primary-foreground/60 text-sm text-center md:text-right">
-                Made with 💗 by 
+                Made with 💗 by  
                 <a 
                   href="https://www.somethingmedia.in/" 
                   target="_blank" 
